47bc417a8204b3ffb468c7308daefc13dd5f3a84
Toby Weston <toby.weston@gmail.com>
2013-02-05 19:59:24 +0000

    switching download-jars.sh from wget to curl, tidy up of unzip location and now remove the downloaded zip after inflation

--- maven/src/script/download-jars.sh
+++ maven/src/script/download-jars.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 # Script to download artifacts 
-#  	- wget 
+#  	- curl
 #	- unzip 
 
 if [ $# -lt 4 ]
@@ -17,7 +17,8 @@ DOWNLOAD_NAME=$4
 if [ -d "$DOWNLOAD_DIR/$DOWNLOAD_NAME" ] ; then
     echo "Directory $DOWNLOAD_DIR/$DOWNLOAD_NAME exists"
 else
-	wget $DOWNLOAD_URL/$DOWNLOAD_ZIP -P $DOWNLOAD_DIR
-	unzip $DOWNLOAD_DIR/$DOWNLOAD_ZIP -d$DOWNLOAD_DIR/$DOWNLOAD_NAME
+	curl $DOWNLOAD_URL/$DOWNLOAD_ZIP > $DOWNLOAD_DIR/$DOWNLOAD_ZIP
+	unzip $DOWNLOAD_DIR/$DOWNLOAD_ZIP -d$DOWNLOAD_DIR
+	rm $DOWNLOAD_DIR/$DOWNLOAD_ZIP
 fi    
 
